bb03c52e98a6bd93be0a7ebaf2d61db22a223c3b
Nur Mohammad Rashed <rashed_huawei@yahoo.com>
2020-09-02 14:05:29 +0200

    commited

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+app.use(cors());
+app.options('*', cors());
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 3000;
+
+app.server = app.listen(port, ()=>{
+    console.log(`Running on port ${port}`);
+});
